3efb690b239b7991ae049542d9d38c63c2319747
vipindevanz <83296815+vipindevanz@users.noreply.github.com>
2021-09-29 18:57:40 +0530

    Added Collections Activity

---
+++ app/src/main/java/com/example/uptimeapp/CollectionsModel.kt
@@ -0,0 +1,7 @@
+package com.example.uptimeapp
+
+data class CollectionsModel(
+
+    val imgUrl : String = "",
+    val desc : String = ""
+)
--- app/src/main/java/com/example/uptimeapp/fragments/BrowseFragment.kt
+++ app/src/main/java/com/example/uptimeapp/fragments/BrowseFragment.kt
@@ -2,31 +2,43 @@ package com.example.uptimeapp.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.uptimeapp.DeepDiveActivity
 import com.example.uptimeapp.R
 import com.example.uptimeapp.TopicActivity
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.fragment_browse.*
 
-class BrowseFragment : Fragment(R.layout.fragment_browse) {
+class BrowseFragment : Fragment(R.layout.fragment_browse), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        collections.setOnClickListener(this)
     }
 
-    private fun openDeepDiveActivity(string : String){
+    override fun onClick(v: View?) {
 
-        val intent = Intent(context, DeepDiveActivity::class.java)
-        intent.putExtra("deepDiveKey", string)
-        startActivity(intent)
-    }
+        var key : String? = null
+        var intent : Intent ? = null
 
-    private fun openTopicActivity(topic : String){
+        if (v is MaterialButton){
 
-        val intent = Intent(context, TopicActivity::class.java)
-        intent.putExtra("deepDiveKey", topic)
+            key = v.text.toString()
+            intent = Intent(context, TopicActivity::class.java)
+
+        } else if (v is TextView){
+            key = v.text.toString()
+            intent = Intent(context, DeepDiveActivity::class.java)
+
+        }
+
+        intent?.putExtra("key", key)
         startActivity(intent)
     }
 }
