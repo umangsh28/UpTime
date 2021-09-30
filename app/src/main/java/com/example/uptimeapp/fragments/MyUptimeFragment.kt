1ae96d335d7bb9cbecd3edafdf79b493d3eb9c64
Kotlin <imreprisal@gmail.com>
2021-09-30 09:10:51 +0530

    commit

--- app/src/main/java/com/example/uptimeapp/fragments/MyUptimeFragment.kt
+++ app/src/main/java/com/example/uptimeapp/fragments/MyUptimeFragment.kt
@@ -1,8 +1,15 @@
 package com.example.uptimeapp.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.uptimeapp.R
+import kotlinx.android.synthetic.main.fragment_my_uptime.*
 
 class MyUptimeFragment : Fragment(R.layout.fragment_my_uptime) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
