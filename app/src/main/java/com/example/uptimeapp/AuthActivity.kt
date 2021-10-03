fe8f49eeb76185e919c5550f1fc71401701d59aa
Kotlin <imreprisal@gmail.com>
2021-10-03 14:54:39 +0530

    Sparks layout

---
+++ app/src/main/java/com/example/uptimeapp/AuthActivity.kt
@@ -0,0 +1,90 @@
+package com.example.uptimeapp
+
+import android.app.Dialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.*
+
+
+class AuthActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+    private val TAG = "GOOGLEAUTH"
+    var mGoogleSignInClient: GoogleSignInClient? = null
+    private var mAuth: FirebaseAuth? = null
+    var dialog: Dialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("282060043265-rfdki1rt9dn8fs9qirf36ulcm11o5i9r.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        dialog = Dialog(this@AuthActivity)
+        dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog!!.setContentView(R.layout.progress_wait)
+        dialog!!.setCanceledOnTouchOutside(false)
+
+        val signInbtn = findViewById<Button>(R.id.google_signIn)
+        signInbtn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if (requestCode == RC_SIGN_IN) {
+            dialog!!.show()
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+                dialog!!.dismiss()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth!!.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+
+                Log.d(TAG, "signInWithCredential:success")
+                val user = mAuth!!.currentUser
+                val i = Intent(this@AuthActivity, MainActivity::class.java)
+                startActivity(i)
+                finish()
+                dialog!!.dismiss()
+
+            }
+            else {
+                dialog!!.dismiss()
+                Toast.makeText(this@AuthActivity, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
